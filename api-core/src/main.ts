259d4ff1b8098b9fdc144193b2ba2460a6d3d70e
Russell Tan <sellnat77@gmail.com>
2025-10-01 23:54:55 -0700

    networking pt 2

--- api-core/src/main.ts
+++ api-core/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://dashboard/',
+    origin: [
+      'http://dashboard:3000/',
+      'http://dashboard:5000/',
+      'http://localhost:3000',
+    ],
   });
   console.log(process.env.PORT);
   await app.listen(3000);
