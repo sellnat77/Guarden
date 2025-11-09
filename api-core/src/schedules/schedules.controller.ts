cb175562fa731d3ce054664f42cd8f47c5616860
Russell Tan <sellnat77@gmail.com>
2025-11-09 11:01:33 -0800

    Npm updates (#24)
    * Bump versions
    
    * Add build step
    
    * Add legacy peer deps
    
    * Bump

--- api-core/src/schedules/schedules.controller.ts
+++ api-core/src/schedules/schedules.controller.ts
@@ -1,24 +1,9 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Param, Delete } from '@nestjs/common';
 import { SchedulesService } from './schedules.service';
-import { CreateScheduleDto } from './dto/create-schedule.dto';
-import { UpdateScheduleDto } from './dto/update-schedule.dto';
 
 @Controller('schedules')
 export class SchedulesController {
   constructor(private readonly schedulesService: SchedulesService) {}
-
-  @Post()
-  create(@Body() createScheduleDto: CreateScheduleDto) {
-    return this.schedulesService.create(createScheduleDto);
-  }
 
   @Get()
   findAll() {
@@ -28,14 +13,6 @@ export class SchedulesController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.schedulesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateScheduleDto: UpdateScheduleDto,
-  ) {
-    return this.schedulesService.update(+id, updateScheduleDto);
   }
 
   @Delete(':id')
