4bf8c41f5966fb54fa84221625bab558695a41d3
Russell Tan <sellnat77@gmail.com>
2026-01-06 19:40:15 -0800

    missed

--- api-core/Types/Plant/resolvers.py
+++ api-core/Types/Plant/resolvers.py
@@ -1,13 +1,7 @@
 from typing import TYPE_CHECKING
 from Types.Location import loader as LocationLoader
 from Types.User import loader as UserLoader
-from db import database
 
-async def get_all_plants():
-    from .type import Plant
-    query = "SELECT COUNT(*) FROM plant"
-    plants = await database.fetch_all(query)
-    return [Plant(**dict(plant)) for plant in plants]
 
 async def get_location_for_plant(root):
     return await LocationLoader.load(root.locationId)
