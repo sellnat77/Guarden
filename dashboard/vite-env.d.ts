39830542cffa47b7a0a8c443ee91a59289afde34
Russell Tan <sellnat77@gmail.com>
2026-03-03 21:12:17 -0800

    Correcting env vars and adding defaults (#91)

--- dashboard/vite-env.d.ts
+++ dashboard/vite-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference types="vite/types/importMeta.d.ts" />
+
+interface ImportMetaEnv {
+  readonly VITE_GD_GRAPHQL_SERVER: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
