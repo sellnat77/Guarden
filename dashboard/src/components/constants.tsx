521d103b18d5c56dcf96cf4556773ed084cfed90
Russell Tan <sellnat77@gmail.com>
2026-03-29 23:42:51 -0700

    fix: if port is present, use port 5000 for backend (#131)

--- dashboard/src/components/constants.tsx
+++ dashboard/src/components/constants.tsx
@@ -1,3 +1,3 @@
 export const GRAPHQL_SERVER =
   import.meta.env.VITE_GD_GRAPHQL_SERVER ||
-  `${window.location.protocol}//${window.location.hostname}:5000`;
+  `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':5000' : ''}`;
