151f19fbebcb293fc72da1a399bcded51bde8931
Russell Tan <sellnat77@gmail.com>
2026-04-19 09:56:30 -0700

    build fixes

--- dashboard/src/components/constants.tsx
+++ dashboard/src/components/constants.tsx
@@ -1,6 +1,6 @@
 export const GRAPHQL_SERVER_CODEGEN =
   "http://localhost:5000";
 
-export const GRAPHQL_SERVER =
-  import.meta.env.VITE_GD_GRAPHQL_SERVER ||
-  `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':5000' : ''}`;
+  export const GRAPHQL_SERVER =
+    import.meta.env.VITE_GD_GRAPHQL_SERVER ||
+    `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':5000' : ''}`;
